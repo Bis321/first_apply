9e08034f8faf6eec91ca987000424288b9e8746c
Bis321 <biswajit.paria@g-ushin.com>
2025-03-31 12:18:38 +0530

    step 9

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const server = app.listen(3000, () => {
 
 // Automatically stop server after 30 seconds
 setTimeout(() => {
-  console.log("Shutting down server...");
+  console.log("Shutting down server..."); 
   server.close(() => {
     console.log("Server stopped.");
     process.exit(0);
